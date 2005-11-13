aab663f84d3018ebf1a3ca9ba0a84d6f0441adac
ianb <devnull@localhost>
2005-11-13 05:24:38 +0000

    Put in proper path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import sys, os
-sys.path.insert(0, os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), 'formencode'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 import pkg_resources
 try:
     import elementtree
