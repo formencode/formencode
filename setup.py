26e42e1904b7ecd8bd6e7d0b65a85bdd013af416
Philipp Hagemeister <phihag@phihag.de>
2012-12-06 20:25:53 +0100

    Change setup.py URL to github

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ FormEncode validates and converts nested structures.  It allows for
 a declarative form of defining the validation, and decoupled processes
 for filling and generating forms.
 
-The official repo is at BitBucket: https://bitbucket.org/formencode/official-formencode/
+The official repo is at GitHub: https://github.com/formencode/formencode
 """,
       classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
