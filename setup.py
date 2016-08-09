6b0775cb71aa373cf15b7416148aa89524a72f50
Chris Lambacher <chris@kateandchris.net>
2016-08-08 23:02:16 -0400

    Trove classifier fix

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='FormEncode',
       classifiers=[
           "Development Status :: 4 - Beta",
            "Intended Audience :: Developers",
-           "License :: OSI Approved :: MIT",
+           "License :: OSI Approved :: MIT License",
            "Programming Language :: Python",
            "Programming Language :: Python :: 2",
            "Programming Language :: Python :: 2.6",
